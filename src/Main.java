cd5b9a594235ad286f8d8395d9a31d09f77d9971
LaTeX Enthusiast <36778031+LaTeXEnthusiast@users.noreply.github.com>
2018-05-21 14:56:22 +0200

    GUI Finished

--- src/Main.java
+++ src/Main.java
@@ -9,21 +9,24 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    private Label typeLabel;
+    private Label typeLabel, operationLabel;
     private ToggleGroup types;
     private RadioButton IntegerType, StringType, DoubleType;
     private Button createTreeButton, addButton, removeButton, searchButton;
     private TextField argumentTextField;
     private HBox mainHBox;
     private Pane container;
-    private VBox mainVBox;
+    private VBox mainVBox, typeVbox, operationVbox;
     private StackPane drawingPane;
+
 
 
     public void start(Stage primaryStage) throws Exception {
 
+        //COMPONENTS
         container = new Pane();
-        typeLabel = new Label();
+        typeLabel = new Label("Type of the tree: ");
+        operationLabel = new Label("Element: ");
         types = new ToggleGroup();
         IntegerType = new RadioButton("Integer");
         StringType = new RadioButton("String");
@@ -31,20 +34,41 @@ public class Main extends Application {
         createTreeButton = new Button("Create tree");
         addButton = new Button("add");
         removeButton = new Button("remove");
-        searchButton = new Button("serach");
+        searchButton = new Button("search");
         argumentTextField = new TextField();
         mainHBox = new HBox();
         mainVBox = new VBox();
+        typeVbox = new VBox();
+        operationVbox = new VBox();
         drawingPane = new StackPane();
 
+        //ADDING CHILDREN
+        types.getToggles().addAll(IntegerType, StringType, DoubleType);
+        container.getChildren().addAll(mainHBox);
+        mainHBox.getChildren().addAll(mainVBox, drawingPane);
+        mainVBox.getChildren().addAll(typeVbox, operationVbox);
+        typeVbox.getChildren().addAll(typeLabel, IntegerType, StringType, DoubleType, createTreeButton);
+        operationVbox.getChildren().addAll(operationLabel, argumentTextField, addButton, removeButton, searchButton);
 
+        //FUNCTIONALITY
         primaryStage.setTitle("BTree");
         primaryStage.setResizable(false);
+        addButton.setDisable(true);
+        removeButton.setDisable(true);
+        searchButton.setDisable(true);
+        argumentTextField.setDisable(true);
+
+        //STYLE
+
         typeLabel.setStyle("-fx-font-size: 20; -fx-font-family: 'Berlin Sans FB'");
-        typeLabel.setText("Type of the tree:");
+        operationLabel.setStyle("-fx-font-size: 20");
 
-        container.getChildren().add(typeLabel);
+        mainVBox.setSpacing(20);
+        argumentTextField.setMaxWidth(100);
 
+        drawingPane.setPrefSize(640, 400);
+
+        //DISPLAYING
         Scene scene = new Scene(container);
         primaryStage.setScene(scene);
         primaryStage.show();
